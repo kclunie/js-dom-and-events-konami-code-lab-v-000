0e21b504515a483405216f80e032764dcc61d22b
github username <github email address>
2019-08-30 22:14:25 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,6 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  //const input = document.body.querySelector('input');
  
 document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
